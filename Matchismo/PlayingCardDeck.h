86f60bd3b57e30fe25f4b11b66007e3ad8b108ea
Alberto Jauregui <bastertwo@gmail.com>
2014-05-20 00:06:37 -0600

    New class PlayingCardDeck and a fix for the PlayingCard

---
+++ Matchismo/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Alberto Jauregui on 5/19/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
