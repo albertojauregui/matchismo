3f6d4c4f16fc47ab3d09aa399dcee45b36b5e404
Alberto Jauregui <bastertwo@gmail.com>
2014-05-19 22:58:18 -0600

    Initial Commit

---
+++ Matchismo/AJCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJCAppDelegate.h
+//  Matchismo
+//
+//  Created by Alberto Jauregui on 5/19/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Alberto Jauregui on 5/19/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AJCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AJCAppDelegate class]));
+    }
+}
