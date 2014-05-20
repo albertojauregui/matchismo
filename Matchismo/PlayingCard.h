c8beb5d578b824de1dd2662883e22af7107599b5
Alberto Jauregui <bastertwo@gmail.com>
2014-05-19 23:47:37 -0600

    New class PlayingCard

---
+++ Matchismo/PlayingCard.h
@@ -0,0 +1,18 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by Alberto Jauregui on 5/19/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@property (nonatomic, strong) NSString *suit;
+@property (nonatomic) NSUInteger rank;
+
++ (NSArray *) validSuits;
+
+@end
---
+++ Matchismo/PlayingCard.m
@@ -0,0 +1,50 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by Alberto Jauregui on 5/19/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@synthesize suit = _suit; // because we implement both the setter and the getter we need to synthetize the property
+
+- (NSString *)contents { // inherits from Card so we are overriding this property getter
+    NSArray *rankStrings = [PlayingCard rankStrings];
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+}
+
+- (void) setSuit:(NSString *)suit { // setter for the suit property
+    if([[PlayingCard validSuits] containsObject:suit]){ // we call the class method here to compare with the suit
+        _suit = suit;
+    }
+}
+
+- (NSString *)suit { // getter for the suit property to prevent use suit with nil value instead we return a question mark
+    return _suit ? _suit : @"?";
+}
+
+- (void) setRank:(NSUInteger)rank {
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
+
+#pragma mark - Class Methods
++ (NSArray *)validSuits { // class method
+    return @[@"♥️", @"♦️", @"♠️", @"♣️"];
+}
+
++ (NSArray *)rankStrings {
+    return @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
+}
+
++ (NSUInteger)maxRank {
+    return [[self rankStrings] count]-1;
+}
+
+
+@end
