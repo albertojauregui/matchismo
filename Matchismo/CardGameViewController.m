c560f7105a7f1ee0a24ca13fc1c451f2873bc760
Alberto Jauregui <bastertwo@gmail.com>
2014-05-20 01:06:31 -0600

    Added flipsLabels to the view

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -9,10 +9,16 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
+
+- (void) setFlipCount:(int)flipCount { // setter for the flipCount property to update also the flipsLabel.text
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
 
 - (IBAction)touchCardButton:(UIButton *)sender {
     if([sender.currentTitle length]){
@@ -24,5 +30,6 @@
                           forState:UIControlStateNormal];
         [sender setTitle:@"A♣️" forState:UIControlStateNormal];
     }
+    self.flipCount++;
 }
 @end
